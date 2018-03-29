e2d2a0fd4ae3b3856a19ffd66f7a417bc8eacb81
Will Washburn <will@tailwindapp.com>
2018-03-29 12:40:08 -0400

    Add parsing for http-refresh-equiv tag

--- src/Canonical.php
+++ src/Canonical.php
@@ -5,7 +5,7 @@ use Symfony\Component\DomCrawler\Crawler;
 /**
  * Given a string of html, return the true canonical url from various tags
  *
- * @author  Will Washburn
+ * @author Will Washburn
  */
 class Canonical
 {
@@ -30,9 +30,10 @@ class Canonical
     protected function getDefaultTags()
     {
         return [
-            ['link[rel="canonical"]', 'href'],
-            ['meta[property="og:url"]', 'content'],
-            ['meta[name="twitter:url"]', 'content'],
+            'canonical'    => ['link[rel="canonical"]', 'href'],
+            'og'           => ['meta[property="og:url"]', 'content'],
+            'twitter'      => ['meta[name="twitter:url"]', 'content'],
+            'http-refresh' => ['meta[http-equiv="refresh"]', 'content'],
         ];
     }
 
@@ -44,7 +45,7 @@ class Canonical
      */
     public function url($body, array $tags = [])
     {
-        if ( !$tags OR !is_array($tags) ) {
+        if (!$tags or !is_array($tags)) {
             $tags = $this->getDefaultTags();
         }
 
@@ -55,20 +56,41 @@ class Canonical
         // probably should do some validation here
         $this->crawler->addContent($body);
 
-        foreach ( $tags as $property ) {
-
+        foreach ($tags as $type => $property) {
             $tag       = $property[0];
             $attribute = $property[1];
 
             $tag = $this->crawler->filter($tag);
 
-            if ( count($tag) == 0 ) {
+            if (count($tag) == 0) {
                 continue;
             }
 
             $url = $tag->attr($attribute);
 
-            if ( $url ) {
+            if ($url) {
+                /*
+                 * If we are using the http-refresh tag, then we need to rip out
+                 * the actual url from the tag.
+                 *
+                 * A http-refresh tag generally looks like this:
+                 *   <meta http-equiv="refresh" content="3;url=http://foobar.com" />
+                 *
+                 * We want to only have the part after the url= piece.
+                 */
+                if ($type == 'http-refresh') {
+                    $re = '/url=(.*)/i';
+
+                    preg_match($re, $url, $matches);
+
+                    $url = $matches[1];
+
+                    // If this fails, we try a different tag
+                    if (!$url) {
+                        continue;
+                    }
+                }
+
                 return $this->cleanUrl($url);
             }
         }
@@ -82,21 +104,20 @@ class Canonical
      * that should not be in the canonical url are not in the canonical url
      *
      * @param $url
+     *
+     * @return bool|null|string|string[]
      */
     private function cleanUrl($url)
     {
-
         //remove utm params via regex replace, and cleanup and artifacts left behind afterwards
         $url = preg_replace('/\?$/', '', preg_replace('/&$/', '', preg_replace('/utm_[^&]+&?/i', '', $url)));
 
         //remove any hash anchors.
         $hash_pos = strpos($url, "#");
-        if ( $hash_pos !== false ) {
+        if ($hash_pos !== false) {
             $url = substr($url, 0, $hash_pos);
         }
 
         return $url;
-
     }
-
 }
--- tests/Test.php
+++ tests/Test.php
@@ -32,7 +32,7 @@ class ExpandLinkTest extends PHPUnit_Framework_TestCase
             ['og_and_link.html', 'http://blog.tailwindapp.com/tailwind-publisher-2-0/'],
             ['partial.html', 'http://blog.tailwindapp.com/tailwind-publisher-2-0/'],
             ['utms.html', 'https://www.etsy.com/listing/237462283/baby-carrying-jacket-baby-carrier-coat'],
+            ['refresh_tag.html', 'http://www.awin1.com/cread.php?awinmid=6220&awinaffid=202819&clickref=3083594506&p=https%3A%2F%2Fwww.etsy.com%2Flisting%2F539434803%2Fgirl-woodland-nursery-nursery-decor-girl%3Fga_order%3Dmost_relevant%26ga_search_type%3Dall%26ga_view_type%3Dgallery%26ga_search_query%3Dbaby%2520girl%2520nursery%26ref%3Dsc_gallery_1%26plkey%3D2c352cb72e3b0737c849359d06df10af4cb14aff%3A539434803%26source%3Daw%26'],
         ];
     }
-
 }
